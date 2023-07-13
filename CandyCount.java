e74ed27de9924c544e3348ce91f22a6fd9a5a6e6
ryanrasal <ryan.beaujot@outlook.fr>
2023-07-13 10:24:29 +0200

    finish java03

--- CandyCount.java
+++ CandyCount.java
@@ -1,14 +1,12 @@
 public class CandyCount {
     public static void main(String[] args) {
-        double money = 12.4;
-        double price = 1.2;
-        int candies = 0;
-        if (money > 0 && price > 0) {
-            while (money - price >= 0) {
-                candies += 1;
-                money -= price;
-            }
-        }
-        System.out.println(candies);
+        String title = " Indiana Jones and the Last Crusade";
+        boolean showMovie = true;
+        int yearMovie = 1989;
+        float rating = 8.2f;
+        System.out.println(title);
+        System.out.println(showMovie);
+        System.out.println(yearMovie);
+        System.out.println(rating);
     }
 }
